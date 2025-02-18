bcf9b4aedbc1101587e1b1156ad0db2cb90e16f6
jieunyy <ina011011@naver.com>
2025-02-18 18:01:39 +0900

    feat: header 클릭시 posts로 nvaigation

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,11 @@
 import { renderProfileLogoButton, setupProfileButton } from "../ProfileLogoButton/ProfileLogoButton.js";
+import { loadPage } from "../../scripts/app.js"; // loadPage 함수 import
 
 export function renderHeader() {
     loadHeaderStyles();
     return `
         <header class="header-container">
-            <h1 class="header-title">아무 말 대잔치</h1>
+            <h1 id="header-title" class="header-title">아무 말 대잔치</h1>
             <div class="profile-btn">
                 ${renderProfileLogoButton()}
             </div>
@@ -14,6 +15,15 @@ export function renderHeader() {
 
 export function setupHeader() {
     setupProfileButton();
+
+    // 헤더 타이틀만 클릭 시 게시글 페이지로 이동
+    const headerTitle = document.getElementById("header-title");
+    if (headerTitle) {
+        headerTitle.addEventListener("click", (e) => {
+            e.stopPropagation(); // 이벤트 버블링 방지
+            loadPage("../pages/posts/posts.js");
+        });
+    }
 }
 
 function loadHeaderStyles() {
